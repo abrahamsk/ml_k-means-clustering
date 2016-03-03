0d721e61532c4436ecdd6c00a8fdcd1eed632184
abrahamsk <abrahamskatie@gmail.com>
2016-03-02 18:34:55 -0800

    euclidean distance function

--- src/exp1.py
+++ src/exp1.py
@@ -7,6 +7,7 @@
 # 3/8/16
 
 from input import *
+import random, numpy as np
 
 """
 Write a program to implement K-means clustering using Euclidean distance, and to
@@ -52,23 +53,66 @@ features_test, labels_test = load_optdigits_data("optdigits/optdigits.test")
 clusters = [i for i in xrange(0, 10)]
 centers = [i for i in xrange(0, 10)]
 
-# initial cluster centers should be chosen at random, with each attribute Ai being an integer in the range [0,16]
-#
-# for i in features_train:
-#     print i,"\n"
-# convert lists to arrays to compute mean for centroid calculation
-# compute new centroid with np.mean
-# features_train, labels_train = convert_data_to_arrays(labels_train, labels_train)
-# features_test, labels_test = convert_data_to_arrays(labels_test, labels_test)
-features_train = np.asarray(features_train)
-labels_train = np.asarray(labels_train)
-features_test = np.asarray(features_test)
-labels_test = np.asarray(labels_test)
 
-centroid = []
-print features_train.shape
-centroid = np.mean(features_train, axis=0)
-print centroid
+###########
+# Functions
+###########
 
-# visualization
+def euclidean_dist(x, y):
+    """
+    L2 (Euclidean) distance
+    Use for distance calculation
+    :param x:
+    :param y:
+    :return:
+    """
+    return np.sqrt(np.sum((x - y) ** 2))
+
+
+def check_cluster_centers(old_cluster, new_cluster):
+    """
+    Stop iterating K-Means when all cluster centers stop changing
+    or if the algorithm is stuck in an oscillation.
+    Use d or d^2 (Euclidean distance) to compute distances between each index of centers
+    :param old_cluster:
+    :param new_cluster:
+    :return false if cluster centers have stopped moving:
+    """
+
+
+#######################################################################
+# Training (run 5 times)
+
+# Outer loop: run 5 times, per hw
+# Inner loop: go until centroid is not moving or until threshold is met
+# (distance between prev centroid and current centroid is .1)
+#######################################################################
+for i in xrange(0, 5):
+    # initial cluster centers should be chosen at random, with each attribute Ai being an integer in the range [0,16]
+    # for each of the 10 centers, generate a length 64 cluster
+    # len 10 with 64 attributes at each of the 10 indices
+    centers = [[random.randint(0, 16) for i in xrange(0, 64)] for j in xrange(0, 10)]
+    centroids = []
+    # Stop iterating K-Means when all cluster centers stop changing
+    # or if the algorithm is stuck in an oscillation.
+    while (check_cluster_centers(centers, centroids)):
+        #######################
+        # Compute new centroids
+        #######################
+        # convert lists to arrays to compute mean for centroid calculation
+        # compute new centroid with np.mean
+        features_train = np.asarray(features_train)
+        labels_train = np.asarray(labels_train)
+        features_test = np.asarray(features_test)
+        labels_test = np.asarray(labels_test)
+        # features_train.shape = (3823, 64)
+        centroids = []
+        # take mean of old centroid, not features_train
+        centroids = np.mean(features_train, axis=0)
+
+###############
+# Visualization
+###############
 # (i*256)/16 to assign to "buckets" for PGM (PGM uses 1-256 instead of 1-16)
+# create 8x8 matrix:
+# print out 8 attributes for each row
