f5b7aaba478d226bd5ebde198efed76158e1ae9b
abrahamsk <abrahamskatie@gmail.com>
2016-03-06 15:22:13 -0800

    Commit before changing empty cluster handling

--- src/exp1.py
+++ src/exp1.py
@@ -42,29 +42,39 @@ You can do this using any matrix-to-bit-map format – e.g., pgm: http://en.wiki
 # Functions
 ###########
 
-def euclidean_dist(instance_features, center):
+def euclidean_dist(x, y):
     """
     L2 (Euclidean) distance
     Use for distance calculation
-    :param x:
-    :param y:
+    :param x: instance_features or clusters, depending on args:
+    :param y: center or clusters, depending on args:
     :return:
     """
-    return np.sqrt(np.sum(((f - c) ** 2 for f, c in zip(instance_features, center))))
+    return np.sqrt(np.sum(((f - c) ** 2 for f, c in zip(x, y))))
     # use center[1] when passing in centers enumerated, to access centers and not identifying cluster #
 
 
-def check_cluster_centers(clusters, new_clusters):
+def initialize_clusters():
+    """
+    Initialize random centers and empty centroids
+    :return:
+    """
+    centers = [[random.randint(0, 16) for i in xrange(0, 64)] for j in xrange(0, 10)]
+    centroids = [[0 for i in xrange(0, 64)] for j in xrange(0, 10)]
+    return centers, centroids
+
+
+def check_cluster_centers(centers, new_centers):
     """
     Stop iterating K-Means when all cluster centers stop changing
     or if the algorithm is stuck in an oscillation.
     Use d or d^2 (Euclidean distance) to compute distances between each index of centers
-    :param clusters:
-    :param new_clusters:
+    :param centers:
+    :param new_centers:
     :return false if cluster centers have stopped moving (or threshold of .1 has been met):
     """
     # check for empty new_clusters (no new centroids during first k-means iteration)
-    if len(new_clusters) == 0:
+    if len(new_centers) == 0:
         return True
 
     distances = []
@@ -74,9 +84,13 @@ def check_cluster_centers(clusters, new_clusters):
 
     # enumerate(thing), where thing is either an iterator or a sequence,
     # returns a iterator that will return (0, thing[0]), (1, thing[1]), (2, thing[2]), etc
-    for i, j in enumerate(clusters):
-        for k, l in enumerate(clusters[i]):
-            distances.append(euclidean_dist(clusters[i][k], new_clusters[i][k]))
+    # for i, j in enumerate(centers):
+    #     for k, l in enumerate(centers[i]):
+    #         distances.append(euclidean_dist(centers[i][k], new_centers[i][k]))
+    for c in centers:
+        for nc in new_centers:
+            # find minimum distance
+            distances.append(euclidean_dist(c, nc))
 
     met_threshold = []
     for d in distances:
@@ -89,6 +103,16 @@ def check_cluster_centers(clusters, new_clusters):
     # if clusters are still moving, keep going
     else:
         return True
+
+
+def sum_squared_error(feature_instances, centroids):
+    """
+    Compute sum squared error from feature instances to center of clusters
+    :param features_train:
+    :param centroids:
+    :return:
+    """
+
 
 
 #######################################################################
@@ -106,82 +130,74 @@ def k_means(features_train, labels_train, features_test, labels_test):
     # initial cluster centers should be chosen at random, with each attribute Ai being an integer in the range [0,16]
     # for each of the 10 centers, generate a length 64 cluster
     # len 10 with 64 attributes at each of the 10 indices
-    centers = [[random.randint(0, 16) for i in xrange(0, 64)] for j in xrange(0, 10)]
-    centroids = [[0 for i in xrange(0, 64)] for j in xrange(0, 10)]
+    centers, centroids = initialize_clusters()
+
     # Stop iterating K-Means when all cluster centers stop changing
     # or if the algorithm is stuck in an oscillation.
-    ########## while (check_cluster_centers(centers, centroids)):
+    do_continue = check_cluster_centers(centers, centroids)
+    while (do_continue):
+        ################################################
+        # Form K clusters by assigning each point to its
+        # closest centroid using Euclidean distance.
+        # Find closest center for each instance
+        # by iterating through centers and finding min
+        # distance from instance to one of the centers.
+        ################################################
+        # K clusters to match K centers
+        # with nested lists for centers and a list of feature instances
+        clusters = [[] for i in xrange(len(centers))]
+        # minimum distances between feature vectors and centers
+        min_dist = []
+        # distances to compare to get minimum
+        compare_dist = []
+        # keep track of center with min distance
+        track_center = []
+        # center with min distance to feature vector
+        min_dist_center = []
 
-    ################################################
-    # Form K clusters by assigning each point to its
-    # closest centroid using Euclidean distance.
-    # Find closest center for each instance
-    # by iterating through centers and finding min
-    # distance from instance to one of the centers.
-    ################################################
-    # K clusters to match K centers
-    clusters = [[] for i in xrange(len(centers))]
-    # minimum distances between feature vectors and centers
-    min_dist = []
-    # distances to compare to get minimum
-    compare_dist = []
-    # keep track of center with min distance
-    track_center = []
-    # center with min distance to feature vector
-    min_dist_center = []
-
-    # distances from instances to centers
-    dists = []
-    # Euclidean distance from features to centers
-    # find minimum distances from f -> c and build clusters
-    # out of features with min distance to center
-    for f in features_train:
-        for c in centers:
-            # find minimum distance
-            dists.append(euclidean_dist(f, c))
-        idx = dists.index(min(dists))
-        clusters[idx].append(f)
-        # clear distances list
+        # distances from instances to centers
         dists = []
+        # Euclidean distance from features to centers
+        # find minimum distances from f -> c and build clusters
+        # out of features with min distance to center
+        for f in features_train:
+            for c in centers:
+                # find minimum distance
+                dists.append(euclidean_dist(f, c))
+            idx = dists.index(min(dists))
+            clusters[idx].append(f)
+            # clear distances list
+            dists = []
 
-    ########################################
-    # Recompute the centroid of each cluster
-    ########################################
-    # convert lists to arrays to compute mean for centroid calculation
-    # compute new centroid with np.mean
-    features_train = np.asarray(features_train)
-    labels_train = np.asarray(labels_train)
-    features_test = np.asarray(features_test)
-    labels_test = np.asarray(labels_test)
-    # features_train.shape = (3823, 64)
-    # random list so length of list is correct
-    centroids = [[0 for i in xrange(0, 64)] for j in xrange(0, 10)]
-    # for each cluster, take mean of of the instances in the cluster to get new centroid
+        ########################################
+        # Recompute the centroid of each cluster
+        ########################################
+        # convert lists to arrays to compute mean for centroid calculation
+        # compute new centroid with np.mean
+        features_train = np.asarray(features_train)
+        labels_train = np.asarray(labels_train)
+        features_test = np.asarray(features_test)
+        labels_test = np.asarray(labels_test)
+        # features_train.shape = (3823, 64)
+        # random list so length of list is correct
+        centroids = [[0 for i in xrange(0, 64)] for j in xrange(0, 10)]
+        # for each cluster, take mean of of the instances in the cluster to get new centroid
 
-    # If a cluster is empty:
-    # Choose a replacement centroid
-    # at random, or from cluster that has highest SSE
-    # for i in clusters:
-    #     if len(i) == 0:  # possible to have zero-length clusters
-    #         print "empty cluster"
-    print len(clusters[0][0])
+        # If a cluster is empty:
+        # Choose a replacement centroid
+        # at random, or from cluster that has highest SSE
+        for i in clusters:
+            if len(i) == 0:  # possible to have zero-length clusters
+                print "Empty cluster"
 
-    # for i in xrange(len(centers)):
-    #     # print clusters[i]
-    #     # print centroids[i]  # all zeroes
-    #     centroids[i] = np.mean(clusters[i], axis=0)  # np.mean(cluster, axis = 0)
-    # print centroids
+        for i in xrange(len(centers)):
+            # print clusters[i]
+            # print centroids[i]  # all zeroes
+            centroids[i] = np.mean(clusters[i], axis=0)  # np.mean(cluster, axis = 0)
+        # print centroids
 
-    # np.sum(((f - c) ** 2 for f, c in zip(instance_features, center)))
-
-    # 1
-    # cluster_counter = 0
-    # for center in centers:
-    #     # each center will still have 64 attributes
-    #     for centroid in centroids:
-    #         centroid.append(np.mean(center, axis=0))
-    #         # cluster_counter += 1
-    #         # print centroids
+        # compute SSE
+        sum_sq_error = sum_squared_error(features_train, centroids)
 
 
 ###############
